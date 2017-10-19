9408ce03ecabfb50d0782175d206da7f3683f38e
dllo <dllo@gebilaowang.local>
2017-10-19 21:02:28 +0800

    添加 用底层写法 for循环 遍历

--- forEach.js
+++ forEach.js
@@ -3,4 +3,15 @@ array.forEach(function (value,index,arr) {
     console.log(value);
     console.log(index);
     console.log(arr);
-})
+});
+
+//底层
+function  Log(value,index,arr) {
+    console.log(value);
+    console.log(index);
+    console.log(arr);
+}
+
+for(var i = 0; i < array.length;i++ ){
+    Log(array[i],i,array);
+}
