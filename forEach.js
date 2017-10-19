5cc56184df9e07d5e04c95c8d6bb0313ef2cbadf
dllo <dllo@gebilaowang.local>
2017-10-19 21:30:53 +0800

    三 添加原型 并输出

--- forEach.js
+++ forEach.js
@@ -15,3 +15,19 @@ function  Log(value,index,arr) {
 for(var i = 0; i < array.length;i++ ){
     Log(array[i],i,array);
 }
+
+//实现
+//添加原型
+Array.prototype.myforEach = function (callback,thisArg) {
+    if(typeof callback !== "function"){
+        throw new TypeError(callback + 'is a not function')
+    }
+    for(var i = 0;i < this.length ; i++){
+        //判断数组下标
+        if (Object.prototype.hasOwnProperty.call(this,i)){
+            callback(this[i],i,this)
+        }
+        callback(this[i], i , this);
+    }
+
+}
