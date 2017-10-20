362c8ea98da90719fa8fa9617803d88b4ac6127b
dllo <dllo@gebilaowang.local>
2017-10-20 09:02:29 +0800

    添加了兼容写法

--- indexOf.js
+++ indexOf.js
@@ -5,3 +5,19 @@ console.log(data.indexOf(5, "3")); // 4 (从3号位开始搜索)
 
 console.log(data.indexOf(4)); // -1 (未找到)
 console.log(data.indexOf("5")); // -1 (未找到，因为5 !== "5")
+
+
+if (typeof Array.prototype.indexOf != "function") {
+    Array.prototype.indexOf = function (searchElement, fromIndex) {
+        var index = -1;
+        fromIndex = fromIndex * 1 || 0;
+
+        for (var k = 0, length = this.length; k < length; k++) {
+            if (k >= fromIndex && this[k] === searchElement) {
+                index = k;
+                break;
+            }
+        }
+        return index;
+    };
+}
